e2e08d10908d088f9331ab84ce6895ab59d06c9b
plima94 <pauloalima1994@gmail.com>
2025-08-22 20:39:21 +0100

    fixing git conflicts

---
+++ src/test/java/com/plimadev/newsletter_api/NewsletterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plimadev.newsletter_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsletterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
