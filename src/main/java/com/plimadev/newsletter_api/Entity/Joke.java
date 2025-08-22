a5316a3f09e1acf62e746629e8bb27931d39931c
plima94 <pauloalima1994@gmail.com>
2025-08-22 22:08:37 +0100

    implemented Joke Entity and Joke Service with getRandomJoke method

---
+++ src/main/java/com/plimadev/newsletter_api/Entity/Joke.java
@@ -0,0 +1,12 @@
+package com.plimadev.newsletter_api.Entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+public class Joke {
+
+    @JsonProperty("value")
+    private String joke;
+
+}
---
+++ src/main/java/com/plimadev/newsletter_api/Service/JokeService.java
@@ -0,0 +1,27 @@
+package com.plimadev.newsletter_api.Service;
+
+import com.plimadev.newsletter_api.Entity.Joke;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+
+@Service
+public class JokeService {
+
+    private final WebClient webClient;
+
+    public JokeService(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder.baseUrl("https://api.chucknorris.io").build();
+    }
+
+    public Mono<String> getRandomJoke(){
+
+        return webClient.get()
+                .uri("/jokes/random")
+                .retrieve()
+                .bodyToMono(Joke.class)
+                .map(Joke::getJoke);
+    }
+
+}
