0cf3fd36f18390642d7496b2d9001b46969fadc6
plima94 <pauloalima1994@gmail.com>
2025-09-04 15:56:40 +0100

    added jpa dependency and created JokeRepository

---
+++ src/main/java/com/plimadev/newsletter_api/Repository/JokeRepository.java
@@ -0,0 +1,14 @@
+package com.plimadev.newsletter_api.Repository;
+
+import com.plimadev.newsletter_api.Entity.Joke.Joke;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface JokeRepository extends JpaRepository<Joke, Long> {
+
+    List<Joke> findAll();
+    Joke findByJokecategory(String category);
+}
